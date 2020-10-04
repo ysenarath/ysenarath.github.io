820140635a9943939de7d6b28a37485a22979d1c
Yasas Senarath <ysenarath.93@gmail.com>
2020-10-03 23:06:42 -0400

    Update project template.

--- src/build.py
+++ src/build.py
@@ -1,7 +1,15 @@
+import json
+
 from staticjinja import Site
 
+with open('src/projects.json', 'r', encoding='utf-8') as fp:
+    projects = json.load(fp)
+
+contexts = [
+    ('projects.html', dict(projects=projects))
+]
 
 if __name__ == "__main__":
-    site = Site.make_site()
+    site = Site.make_site(contexts=contexts)
     # enable automatic reloading
     site.render(use_reloader=True)
