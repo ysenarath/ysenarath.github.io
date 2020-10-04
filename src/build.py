f1862f0cf95baf2e25602c6035f598890e7bf33b
Yasas Senarath <ysenarath.93@gmail.com>
2020-10-04 14:08:53 -0400

    Update research page.

--- src/build.py
+++ src/build.py
@@ -5,11 +5,15 @@ from staticjinja import Site
 with open('src/projects.json', 'r', encoding='utf-8') as fp:
     projects = json.load(fp)
 
+with open('src/research.json', 'r', encoding='utf-8') as fp:
+    research = json.load(fp)
+
 with open('src/timeline.json', 'r', encoding='utf-8') as fp:
     timeline = json.load(fp)
 
 contexts = [
     ('projects.html', dict(projects=projects)),
+    ('research.html', dict(research=research)),
     ('timeline.html', dict(timeline=timeline)),
 ]
 
